04b96bd24399344ff9b1a8996f219d283384af8c
Maria Cotos <maria.cotos7@yahoo.com>
2020-06-08 14:34:29 +0300

    gigaixir

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,11 @@ use Mix.Config
 config :countdown, CountdownWeb.Endpoint,
   load_from_system_env: true,
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  http: [port: {:system, "PORT"}], 
+  server: true,
+  secret_key_base: "${SECRET_KEY_BASE}",
+  url: [host: "${APP_NAME}.gigalixirapp.com", port: 443]
 
 # Do not print debug messages in production
 config :logger, level: :info
